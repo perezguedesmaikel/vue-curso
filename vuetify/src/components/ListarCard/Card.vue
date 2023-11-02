ccbc930d4ea4811365cd53e71008127893ea1b64
perezguedesmaikel <perezguedesmaikel@gmail.com>
2023-11-02 15:04:05 -0400

    v-bind

--- vuetify/src/components/ListarCard/Card.vue
+++ vuetify/src/components/ListarCard/Card.vue
@@ -1,4 +1,6 @@
 <script lang="ts" setup>
+import {ref} from 'vue';
+
 defineProps(
   {
     name: {
@@ -14,12 +16,20 @@ defineProps(
     }
   }
 );
+const hasError = ref(false);
+
+function changeHasError() {
+  hasError.value = !hasError.value;
+}
 </script>
 
 <template>
-  <v-card :subtitle="apellidos" :title="name" text="...">
+  <v-card :subtitle="apellidos" :title="name" text="Texto de ejemplo..." v-bind:class="{
+    'text-green':hasError
+  }">
     <v-card-actions>
       <v-btn @click="action">Click me</v-btn>
+      <v-btn class="bg-red" @click="changeHasError">Change Status</v-btn>
     </v-card-actions>
   </v-card>
 </template>
